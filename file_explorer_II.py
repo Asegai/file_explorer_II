064587b91b14b3981cbf94ff2512f9c41310f678
Asegai <144498317+Asegai@users.noreply.github.com>
2024-06-28 21:56:19 +0530

    added exception for permission error and opens files
    now if you get a permission error the explorer will ask you to elevate and if you don't it'll1 tell you the file isn't accessible , also clicking on files now opens them

--- file_explorer_II.py
+++ file_explorer_II.py
@@ -1,14 +1,17 @@
 import os
 import tkinter as tkr
 from tkinter import ttk
+import ctypes
+import subprocess
+import tkinter.messagebox as mb
+import sys
+
 
 class FileExplorer(tkr.Tk):
     def __init__(self):
         super().__init__()
-
         self.title("File Explorer")
         self.geometry("800x600")
-    
 
         self.tree = ttk.Treeview(self)
         self.tree.pack(side=tkr.LEFT, fill=tkr.BOTH, expand=True)
@@ -27,7 +30,6 @@ class FileExplorer(tkr.Tk):
         self.tree.heading("type", text="Type", anchor=tkr.W)
 
         self.load_directory(os.path.expanduser("~"))
-
         self.tree.bind("<Double-1>", self.on_double_click)
 
     def load_directory(self, path):
@@ -35,6 +37,14 @@ class FileExplorer(tkr.Tk):
 
         parent_node = self.tree.insert("", "end", text=path, open=True, values=("", ""))
         self.populate_tree(parent_node, path)
+
+    def is_admin(self):
+        """Check if the current process has administrative privileges."""
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+
 
     def populate_tree(self, parent_node, path):
         try:
@@ -45,19 +55,35 @@ class FileExplorer(tkr.Tk):
                 if os.path.isdir(item_path):
                     dirs.append(item)
                 else:
-                    files.append((item, os.path.getsize(item_path)))
+                    extension = os.path.splitext(item)[1][1:] if os.path.splitext(item)[1] else "Unknown"
+                    files.append((item, os.path.getsize(item_path), extension))
             
             dirs.sort()
-            files.sort(key=lambda x: x[0])
+            files.sort(key=lambda x: (x[2], x[0]))
             
             for dir in dirs:
                 node = self.tree.insert(parent_node, "end", text=dir, values=("", "Folder"))
                 self.tree.insert(node, "end")
             
-            for file, size in files:
-                self.tree.insert(parent_node, "end", text=file, values=(self.format_size(size), "File"))
+            for file, size, extension in files:
+                self.tree.insert(parent_node, "end", text=file, values=(self.format_size(size), extension.upper() if extension != "Unknown" else extension))
         except PermissionError:
-            pass
+            if not self.is_admin() and self.try_run_as_admin():
+                return
+            else:
+                mb.showerror("Access Denied", "You do not have permission to access this directory.")
+
+    
+    def try_run_as_admin(self):
+        """Attempt to re-run the application with admin rights only if not already running as admin."""
+        if self.is_admin():
+            return True
+        try:
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+            sys.exit(0)  
+        except Exception as e:
+            print(f"Error elevating privileges: {e}")
+            return False
 
     def on_double_click(self, event):
         item_id = self.tree.selection()[0]
@@ -65,6 +91,8 @@ class FileExplorer(tkr.Tk):
         if os.path.isdir(path):
             self.tree.delete(*self.tree.get_children(item_id))
             self.populate_tree(item_id, path)
+        else:
+            os.startfile(path)
 
     def get_full_path(self, item_id):
         path = self.tree.item(item_id, "text")
